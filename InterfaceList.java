bd9244e501572326883f11662c8eda21f66284cb
homepunk <nikita.storozhenko@gmail.com>
2016-12-03 00:16:42 +0200

    Delete InterfaceList.java

--- InterfaceList.java
+++
@@ -1,9 +0,0 @@
-public interface InterfaceList <T> {
-    void add (T T);
-    T get (int position);
-    void remove(int index);
-
-    boolean contain (T T);
-    int size();
-    boolean isEmpty();
-}
